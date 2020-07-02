59651d436c4ab8319f4f97828e7e0055cfed0e3b
Sebastian Giraldo <sebassgb@users.noreply.github.com>
2020-07-02 02:41:50 +0200

    Update Multi_class_classifier_Rock_Paper_Scissors.py

--- Multi_class_classifier_Rock_Paper_Scissors.py
+++ Multi_class_classifier_Rock_Paper_Scissors.py
@@ -5,37 +5,22 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from os import getcwd
 
 def get_data(filename):
-  # You will need to write code that will read the file passed
-  # into this function. The first line contains the column headers
-  # so you should ignore it
-  # Each successive line contians 785 comma separated values between 0 and 255
-  # The first value is the label
-  # The rest are the pixel values for that picture
-  # The function will return 2 np.array types. One with all the labels
-  # One with all the images
-  #
-  # Tips: 
-  # If you read a full line (as 'row') then row[0] has the label
-  # and row[1:785] has the 784 pixel values
-  # Take a look at np.array_split to turn the 784 pixels into 28x28
-  # You are reading in strings, but need the values to be floats
-  # Check out np.array().astype for a conversion
- with open(filename) as training_file:
-    csv_reader = csv.reader(training_file, delimiter=',')
-    first_line = True
-    temp_images = []
-    temp_labels = []
-    for row in csv_reader:
-        if first_line:
-            first_line = False
-        else:
-            temp_labels.append(row[0])
-            image_data = row[1:785]       
-            image_data_as_array = np.array_split(image_data, 28)        
-            temp_images.append(image_data_as_array)
-            images = np.array(temp_images).astype('float')
-            labels = np.array(temp_labels).astype('float')
+
+# Each successive line contians 785 comma separated values between 0 and 255
+# The first value is the label
+# The rest are the pixel values for that picture
+# The function will return 2 np.array types. One with all the labels
+# One with all the images
+    my_arr = np.loadtxt(filename, delimiter=',', skiprows=1)
+    # get label & image arrays
+    labels = my_arr[:,0].astype('int')
+    images = my_arr[:,1:]
+    # reshape image from 784 to (28, 28)
+    images = images.astype('float').reshape(images.shape[0], 28, 28)
+    # just in case to avoid memory problem
+    my_arr = None
     return (images, labels)
+
 
 path_sign_mnist_train = f"{getcwd()}/../tmp2/sign_mnist_train.csv"
 path_sign_mnist_test = f"{getcwd()}/../tmp2/sign_mnist_test.csv"
@@ -52,11 +37,11 @@ print(testing_labels.shape)
 # (27455, 28, 28)
 # (27455,)
 # (7172, 28, 28)
-# (7172)
+# (7172,)
 
-
-training_images = "/tmp/rps/"
-testing_images = "/tmp/rps-test-set/"
+# Hint: np.expand_dims
+training_images = np.expand_dims(training_images, axis = 3)
+testing_images = np.expand_dims(testing_images, axis = 3)
 
 # Create an ImageDataGenerator and do Image Augmentation
 train_datagen = ImageDataGenerator(rescale = 1./255,
@@ -65,25 +50,9 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    height_shift_range=0.2,
                                    shear_range=0.2,
                                    zoom_range=0.2,
-                                   horizontal_flip=True,
                                    fill_mode='nearest')
 
-
 validation_datagen = ImageDataGenerator(rescale = 1./255)
-
-train_generator = training_datagen.flow_from_directory(
-        train_datagen,
-        target_size=(150,150),
-        class_mode='categorical',
-  batch_size=126
-)
-
-validation_generator = validation_datagen.flow_from_directory(
-        validation_datagen,
-        target_size=(150,150),
-        class_mode='categorical',
-  batch_size=126
-)
 
     
 # Keep These
@@ -94,36 +63,31 @@ print(testing_images.shape)
 # (27455, 28, 28, 1)
 # (7172, 28, 28, 1)
 
-# Define the model
-# Use no more than 2 Conv2D and 2 MaxPooling2D
+# Defining the model
+# no more than 2 Conv2D and 2 MaxPooling2D
 model = tf.keras.models.Sequential([
-    # This is the first convolution
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    # The second convolution
+# Your Code Here
     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
-    # The third convolution
+# The fourth convolution
     tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
-    # The fourth convolution
-    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    # Flatten the results to feed into a DNN
+# Flatten the results to feed into a DNN
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dropout(0.5),
     # 512 neuron hidden layer
     tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(3, activation='softmax')
-    ])
+    tf.keras.layers.Dense(25, activation='softmax')
+])
     
-model.summary()
-    
+#model.summary()
+  
 # Compile Model. 
-model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the Model
-history = model.fit_generator(train_generator, epochs=15, steps_per_epoch=20, validation_data = validation_generator, verbose = 1, validation_steps=3)
+history = model.fit_generator(train_datagen.flow(training_images, training_labels, batch_size=32),
+epochs=15, steps_per_epoch=600, validation_data = validation_datagen.flow(testing_images, testing_labels, batch_size = 32),
+verbose = 1, validation_steps=3)
 
 model.evaluate(testing_images, testing_labels, verbose=0)
 
